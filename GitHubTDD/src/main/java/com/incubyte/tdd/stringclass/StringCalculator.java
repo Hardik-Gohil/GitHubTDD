dbe5876196238a3ddf766b49701d6662d6ca93f9
Hardik-Gohil <Hardik-Gohil@LAPTOP-M6Q80UV4>
2020-07-05 15:59:19 +0530

    Step 1: Created Add(String numbers) method and test cases for empty string and one & two numbers string

--- GitHubTDD/src/main/java/com/incubyte/tdd/stringclass/StringCalculator.java
+++ GitHubTDD/src/main/java/com/incubyte/tdd/stringclass/StringCalculator.java
@@ -1,5 +1,17 @@
 package com.incubyte.tdd.stringclass;
 
+import org.springframework.util.StringUtils;
+
 public class StringCalculator {
 
+	public static int Add(String numbers) {
+		if (!StringUtils.hasLength(numbers)) {
+			return 0;
+		} else if (numbers.length() == 1) {
+			return Integer.parseInt(numbers);
+		} else {
+			String[] number = numbers.split(",");
+			return Integer.parseInt(number[0]) + Integer.parseInt(number[1]);
+		}
+	}
 }
--- GitHubTDD/src/test/java/com/incubyte/tdd/stringclasstest/StringCalculatorTest.java
+++ GitHubTDD/src/test/java/com/incubyte/tdd/stringclasstest/StringCalculatorTest.java
@@ -1,14 +1,26 @@
 package com.incubyte.tdd.stringclasstest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+
+import com.incubyte.tdd.stringclass.StringCalculator;
 
 class StringCalculatorTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void sumWithEmptyNumbers() {
+		assertEquals(0, StringCalculator.Add(""));
+	}
+
+	@Test
+	void sumWithOneNumbers() {
+		assertEquals(1, StringCalculator.Add("1"));
+	}
+
+	@Test
+	void sumWithTwoNumbers() {
+		assertEquals(3, StringCalculator.Add("1,2"));
 	}
 
 }
